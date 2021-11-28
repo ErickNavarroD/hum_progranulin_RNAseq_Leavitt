6aa1d54c143d50571d00bcf05355a205d57b9d36
g_netto <giuliano.netto@biome-hub.com>
2021-11-28 14:46:51 -0800

    check outliers + add GSEA

--- code/utils.R
+++ code/utils.R
@@ -31,3 +31,36 @@ make_dir <- function(.path) {
     dir.create(.path, recursive = T)
   }
 }
+
+plot_gsea <- function(df, N = 25) {
+  df %>% 
+    arrange(p.adjust) %>% 
+    head(N) %>% 
+    mutate(
+      Description = fct_reorder(
+        Description,
+        enrichmentScore
+      ),
+      x = map_dbl(core_enrichment, ~ {
+        str_split(.x, fixed("/")) %>% 
+          (
+            \(k) length(k[[1]])
+          )
+      }),
+      .gene_ratio = x/setSize
+    ) %>% 
+    ggplot(aes(enrichmentScore, Description)) +
+    geom_point(
+      aes(color = p.adjust, size = .gene_ratio)
+    ) +
+    theme_bw() +
+    labs(x = "Enrichment Score",
+         y = NULL, 
+         color = "p adjusted",
+         size = "Gene Ratio") +
+    scale_color_viridis_b(
+      breaks = scales::pretty_breaks(7)
+    ) +
+    theme(legend.key.height = unit(1.2, "cm"))
+}
+
